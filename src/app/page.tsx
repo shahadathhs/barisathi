f71eb7f59f2abe8fe3aa7e46124e2ca3af80cdff
shahadathhs <shahadathhossensajib732@gmail.com>
2025-03-02 08:18:58 +0600

    chore: acernitiy ui added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { AnimatedTestimonialsDemo } from "@/ui/AnimatedTestimonialsDemo";
+import { MarqueeDemo } from "@/ui/MarqueeDemo";
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <div className="w-full h-screen flex justify-center items-center">
+        <MarqueeDemo />
+      </div>
+      <AnimatedTestimonialsDemo />
     </div>
   );
 }
