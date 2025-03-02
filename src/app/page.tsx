10b49edbc01094f297d765665475072656b02cf7
shahadathhs <shahadathhossensajib732@gmail.com>
2025-03-02 08:38:08 +0600

    chore: removed magic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
+import { AnimatedTestimonialsDemo } from "@/components/sections/AnimatedTestimonialsDemo";
 import { Button } from "@/components/ui/button";
-import { AnimatedTestimonialsDemo } from "@/ui/AnimatedTestimonialsDemo";
-import { MarqueeDemo } from "@/ui/MarqueeDemo";
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
-      <div className="w-full h-screen flex justify-center items-center">
-        <MarqueeDemo />
-      </div>
       <AnimatedTestimonialsDemo />
     </div>
   );
