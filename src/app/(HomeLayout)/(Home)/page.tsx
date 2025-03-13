0adac7caa00b220a2d0171835f2a3cb7360290a6
shahadathhs <shahadathhossensajib732@gmail.com>
2025-03-13 08:25:58 +0600

    feat: updated favicon and global meta info

--- src/app/(HomeLayout)/(Home)/page.tsx
+++ src/app/(HomeLayout)/(Home)/page.tsx
@@ -1,17 +1,9 @@
-import { AnimatedTestimonialsDemo } from "@/components/sections/AnimatedTestimonialsDemo";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="min-h-screen py-8 md:py-16 px-4 md:px-8 lg:px-12">
       <Button>Click me</Button>
-      <AnimatedTestimonialsDemo />
-      <Button>Click me</Button>
-      <AnimatedTestimonialsDemo />
-      <Button>Click me</Button>
-      <AnimatedTestimonialsDemo />
-      <Button>Click me</Button>
-      <AnimatedTestimonialsDemo />
     </main>
   );
 }
